b121f9a210496fcdba3bd0dd1011e513d92e7467
jpcirqueira <jpcirqueira81@gmail.com>
2021-06-08 19:37:03 -0300

    cutomize admin settings panel.

---
+++ api/admin/src/components/LeftMenu/LeftMenuFooter/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Wrapper from './Wrapper';
+
+function LeftMenuFooter() {
+  // PROJECT_TYPE is an env variable defined in the webpack config
+  // eslint-disable-next-line no-undef
+
+
+  return (
+    <Wrapper>
+      <div className="poweredBy">
+        <a key="website" href="https://strapi.io" target="_blank" rel="noopener noreferrer">
+          React Avançado
+        </a>
+      </div>
+    </Wrapper>
+  );
+
+}
+
+export default LeftMenuFooter;
